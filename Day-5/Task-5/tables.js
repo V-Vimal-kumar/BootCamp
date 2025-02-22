87056a413086fcb3c95e065fd64f311d0c88163c
V-Vimal-kumar <127484073+V-Vimal-kumar@users.noreply.github.com>
2025-02-22 16:14:22 +0530

    Delete Day-5/Task-5 directory

--- Day-5/Task-5/tables.js
+++
@@ -1,9 +0,0 @@
-const num = parseInt(process.argv[2]);
-
-if (isNaN(num)) {
-    console.log("Usage: node script.js <number>");
-} else {
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${num} x ${i} = ${num * i}`);
-    }
-}
